1314ca52039d687aecf4f06bd9f44e56423f4545
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-06 01:26:09 +0700

    chore: add serializing object with decorator

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -23,32 +23,27 @@ video_put_args.add_argument("name", type=str, help="Name of the video", required
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Number of likes on the video", required=True)
 
-videos = {}
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
-
-def handle_non_existing_video(video_id):
-    if video_id not in videos:
-        abort(404, message="Video id = {} is not in the data".format(video_id))
-
-
-def handle_existing_video(video_id):
-    if video_id in videos:
-        abort(409, message="Video id = {} already exist".format(video_id))
 
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        handle_non_existing_video(video_id)
+        result = VideoModel.query.get(id=video_id)
         return videos[video_id]
 
     def put(self, video_id):
-        handle_existing_video(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
 
     def delete(self, video_id):
-        handle_non_existing_video(video_id)
         del videos[video_id]
         return "".format(video_id), 204
 
