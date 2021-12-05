19f24becde69f493bf83eb60e93bd812ba231244
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-05 16:27:07 +0700

    feat: add handling delete data

--- src/main.py
+++ src/main.py
@@ -17,15 +17,26 @@ def handle_non_existing_video(video_id):
         abort(404, message="Video id = {} is not in the data".format(video_id))
 
 
+def handle_existing_video(video_id):
+    if video_id in videos:
+        abort(409, message="Video id = {} already exist".format(video_id))
+
+
 class Video(Resource):
     def get(self, video_id):
         handle_non_existing_video(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        handle_existing_video(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        handle_non_existing_video(video_id)
+        del videos[video_id]
+        return "".format(video_id), 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- src/test.py
+++ src/test.py
@@ -2,11 +2,21 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response_put = requests.put(BASE + "video/12", {"name": "hasan", "views": 12, "likes": 12})
-print(response_put.json())
+datas = [
+    {"name": "hasan", "views": 12, "likes": 12},
+    {"name": "hasen", "views": 13, "likes": 10},
+    {"name": "hason", "views": 14, "likes": 11}
+]
+
+for ID, data in enumerate(datas):
+    response = requests.put(BASE + "video/{}".format(ID), data)
+    print(response.json())
+
 input()
-response_get = requests.get(BASE + "video/12")
-print(response_get.json())
+response = requests.delete(BASE + "video/0")
+print(response)
 input()
-response_get = requests.get(BASE + "video/1")
-print(response_get.json())
+for i in range(len(data)):
+    response = requests.get(BASE + "video/{}".format(i))
+    print(i, response.json())
+
