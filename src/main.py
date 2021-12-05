9c3a6ca7c4b4ee85bf4d105ae7649a24505d72c3
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-05 13:44:35 +0700

    chore: handling request and storing data in memory

--- src/main.py
+++ src/main.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "hasan": {
+        "age": 21,
+        "gender": "male"
+    },
+    "fulanah": {
+        "age": 25,
+        "gender": "female"
+    }
+}
 
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"name": name, "test": test}
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form)
+        return {}
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/test.py
+++ src/test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response_get = requests.get(BASE + "helloworld/hasan/12")
+response_get = requests.put(BASE + "video/12", {"likes": 12})
 print(response_get.json())
