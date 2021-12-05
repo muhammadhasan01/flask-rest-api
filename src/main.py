7836e8ee6278a311b9c6df584d6010c63cbe2519
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-05 13:53:10 +0700

    feat: add argument handling parser

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "hasan": {
-        "age": 21,
-        "gender": "male"
-    },
-    "fulanah": {
-        "age": 25,
-        "gender": "female"
-    }
-}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Number of likes on the video", required=True)
 
 videos = {}
 
@@ -23,8 +17,9 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
-        print(request.form)
-        return {}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id]
 
 
 api.add_resource(Video, "/video/<int:video_id>")
